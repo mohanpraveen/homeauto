92d2d2580613c6731d47382adeafafd4464c4f4e
Praveen Mohan <mohanpraveen@yahoo.com>
2017-12-25 23:53:25 +0530

    fixed the jar file loc

--- ipcheck.sh
+++ ipcheck.sh
@@ -4,6 +4,6 @@ echo "Pulling Git Repo"
 cd /home/pi/IP-tracker/homeauto
 git pull
 echo "Compiling Java class"
-/usr/bin/javac -classpath /home/pi/IP-tracker/javax.mail.tar:. IPChecker.java
+/usr/bin/javac -classpath /home/pi/IP-tracker/homeauto/javax.mail.jar:. IPChecker.java
 echo "Running java class"
-/usr/bin/java -classpath /home/pi/IP-tracker/javax.mail.tar:. IPChecker
+/usr/bin/java -classpath /home/pi/IP-tracker/homeauto/javax.mail.jar:. IPChecker
