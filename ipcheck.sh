bead70986b1773f440ab048c92ef73d6f6009b02
mohanpraveen <34830523+mohanpraveen@users.noreply.github.com>
2017-12-25 23:47:39 +0530

    modified script to pull latest from git

--- ipcheck.sh
+++ ipcheck.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+echo "Pulling Git Repo"
+cd /home/pi/IP-tracker/homeauto
+git pull
 echo "Compiling Java class"
 /usr/bin/javac -classpath /home/pi/IP-tracker/javax.mail.tar:. IPChecker.java
 echo "Running java class"
